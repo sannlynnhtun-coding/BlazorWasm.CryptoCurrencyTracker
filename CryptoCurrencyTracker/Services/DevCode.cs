5fbf4e49782c56e04b29ad617cf4d5356255233d
Sann Lynn Htun <sannlynnhtun.error@gmail.com>
2022-08-13 15:23:31 +0630

    -

---
+++ CryptoCurrencyTracker/Services/DevCode.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json;
+
+namespace CryptoCurrencyTracker.Services
+{
+    public static class DevCode
+    {
+        public static string ToJson(this object obj)
+        {
+            return JsonSerializer.Serialize(obj);
+        }
+    }
+}
