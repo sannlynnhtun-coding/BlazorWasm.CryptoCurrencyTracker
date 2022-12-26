9cf68b3c611a8ea7e7928fcb54feb311d751a928
Sann Lynn Htun <sannlynnhtun.error@gmail.com>
2022-12-26 16:29:58 +0630

    -

--- CryptoCurrencyTracker/Models/HistoricalChartModel.cs
+++ CryptoCurrencyTracker/Models/HistoricalChartModel.cs
@@ -2,6 +2,7 @@
 {
     public class HistoricalChartModel
     {
+        public bool is_success { get; set; }
         public List<List<double>> prices { get; set; }
         public List<List<double>> market_caps { get; set; }
         public List<List<double>> total_volumes { get; set; }
